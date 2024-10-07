3ccc0c9b3bb6a357293b2e041a5e11aaf29aac7c
jafarvali1 <jafarvali99@gmail.com>
2024-10-07 15:06:52 -0700

    images

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import heroImage from '../../../public/images/hero-image.png'
+
 
 const HeroSection = () => {
   return (
@@ -64,7 +66,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#000000] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/porfolio/images/hero-image.png"
+              src={heroImage}
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={500}
